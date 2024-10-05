6e250f204e3fb829f6626bf47aa8ca68d2120208
Bhavana Goad <bhavana-dev>
2024-10-05 22:42:11 +0530

    added responsiveness

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import './style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCrown } from '@fortawesome/free-solid-svg-icons/faCrown'
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons/faChevronDown'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons/faWhatsapp'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram'
+
+
 export default function Index() {
     let [isDistShow , setIsDistShow] =  useState(false)
     let [isContactShow , setIsContactShow] =  useState(false)
@@ -13,43 +16,56 @@ export default function Index() {
         setIsDistShow(!isDistShow)
     }
     let HandleClickContact= () =>{
+        console.log("hhhh");
+        
         setIsContactShow(!isContactShow)
     }
+    console.log(isContactShow);
+    
 
   return (
-    <div className='legend'>
-        <div className='items'>
-            <h2 style={{color:'#8e5e0e',textDecoration:'underline',marginLeft:'50px'}}>Vashnavi Trading Company</h2>
-            <button><a href='https://www.instagram.com/accounts/login/' target='_blank'> Instagram</a> </button>
-            <button ><a href='https://www.facebook.com/people/Kingenergydrink/100092728865950/' target='_blank'>Facebook</a></button>
-            
-            <div className='toggle'>
-            <button>For Distribution <span onClick={HandleClickDistribute}><FontAwesomeIcon icon={faChevronDown} /></span></button>
-                {
-                    isDistShow ? <ul className='ulData'>
-                    <li>Work: 8340840334</li>
-                    <li>Phone: 083409384</li>
-                    <a><li>https://vaishnavi.trading.vercel.com</li></a>
+    <div className="container">
+  
 
-                </ul> : null
-                }
-                
-            </div>
-            <button><a href="https://wa.me/7869546499" target='_blank'>Whatsapp</a> </button>
-            <div>
-            <button>Contact <span onClick={HandleClickContact}><FontAwesomeIcon icon={faChevronDown} /></span></button>
-                {
-                    isContactShow ? <ul className='ulData'>
-                    <li>Work: 8340840334</li>
-                    <li>Phone: 083409384</li>
-                    <a><li>https://vaishnavi.trading.vercel.com</li></a>
+        <div className="col">
+            <a href="#" className="fb btnCls">
+            <FontAwesomeIcon icon={faInstagram} />  Instagram
+            </a>
+            <a href="#" className="fb btnCls">
+            <FontAwesomeIcon icon={faFacebook} />  Facebook
+            </a>
+            <a href="#" className="google btnCls" onClick={HandleClickDistribute}>Distributer</a>
+            {
+                isDistShow ?
+                <div className="content">
+                    <ul className='ulData'>
+                        <li>Work: 8340840334</li>
+                        <li>Phone: 083409384</li>
 
-                </ul> : null
-                }
-            </div>
-            
+                    </ul>
+                </div>
+                : null
+            }
+            <a href="#" className="twitter btnCls">
+            <FontAwesomeIcon icon={faWhatsapp} />   Whatsapp
+            </a>
+            <a href="#" className="google btnCls" onClick={HandleClickContact}>Contact</a>
+            {
+                isContactShow ?
+                <div className="content">
+                    <ul className='ulData'>
+                        <li>Work: 8340840334</li>
+                        <li>Phone: 083409384</li>
+                        <a><li>https://vaishnavi.trading.vercel.com</li></a>
+
+                    </ul>
+                </div>
+                : null
+            }
             
         </div>
+
+     
     </div>
   )
 }
